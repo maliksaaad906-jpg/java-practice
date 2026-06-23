3d09c13e9bf2c39a8ed010e66546c270c2d77f5d
maliksaaad906-jp <maliksaaad906@gmail.com>
2026-06-23 12:16:49 +1000

    learned logical operator

---
+++ logicalOperator.java
@@ -0,0 +1,13 @@
+public class logicalOperator {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+        int c= 4;
+        int d = 9;
+
+        boolean result = a > b && c>d;
+
+        System.out.println(result);
+        System.out.println(!result);
+    }
+}
--- typeconversion.java
+++ typeconversion.java
@@ -3,12 +3,21 @@ public class typeconversion {
         byte num1 = 20;
         int a = num1; 
         float b = num1;
+      
         
-        // int c = b; cannot be conveted again to int rather we have to use casting 
+        // int c = b; 
+        // cannot be conveted again to int rather we have to use casting 
+        //(int)b convert the b to int first and then assign it to c 
+        // which is the right way to assigning float to int , byte to int 
         int c = (int) b;
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
+
+        byte g =10;
+        byte h = 9;
+        int i = ++g;
+        System.out.println(i);
 
     }
 }
