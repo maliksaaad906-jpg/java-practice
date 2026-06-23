0509ab8acf64721499d8238b064d866e27fbf4b0
maliksaaad906-jp <maliksaaad906@gmail.com>
2026-06-23 23:27:15 +1000

    learned printing pattens using nested loops

---
+++ pattern.java
@@ -0,0 +1,17 @@
+public class pattern {
+    public static void main(String[] args) {
+        int n = 9;
+        for (int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                if (  i == j || (j+i)== (n+1) || j == (n/2 + 1 ) || i == (n/2 + 1) ) {
+                    System.out.print("* ");
+                }
+                else
+                    System.out.print("  ");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
